5b7a5e6a3445e05ae281e36c67101cc0a77b27c7
Csa Detec <51001796+csadetec@users.noreply.github.com>
2020-03-02 21:47:32 -0300

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -16,7 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {return { api: 'comeve 18-02' }})
+Route.get('/', () => {return { api: 'comeve sera q ja update autmatico' }})
 Route.post('/authenticate', 'AuthController.authenticate')
 
 Route.group(() => {
